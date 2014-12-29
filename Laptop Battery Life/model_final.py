cf8ef6c6da28061271e00bf037a7ee629ac390ae
keshr3106 <keshav.ramaswamy@gmail.com>
2014-12-29 00:52:25 -0500

    Laptop Battery Life

---
+++ Laptop Battery Life/model_final.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 28 23:37:01 2014
+
+@author: HP
+"""
+
+inp = float(raw_input())
+
+if int(inp) < 4:
+    op = 2.00 * inp
+else:
+    op = 8.00
+    
+print op
